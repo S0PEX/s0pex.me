b2b434aaa5040e007684e84961a7bc850644142c
Artur Komaristych <artur.komaristych@gmail.com>
2024-11-04 11:00:53 +0100

    fix(consts): fix typo in SITE_DESCRIPTION

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,5 @@
 export const SITE_TITLE = 'Artur Komaristych'
 export const SITE_DESCRIPTION =
   "Passionate software developer pursuing a master's in computer science at the University of Cologne. " +
-  'I enjoy solving complex problems and staying active through badminton, bouldering, and climbing. Discover more about my skills and experiences'
+  'I enjoy solving complex problems and staying active through badminton, bouldering, and climbing.' +
+  'Discover more about my skills and experiences on my website!'
