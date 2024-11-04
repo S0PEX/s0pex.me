e6515e41abbff6047610997dff0de36d560f1162
Artur Komaristych <artur.komaristych@gmail.com>
2024-11-04 11:00:52 +0100

    refactor(consts): update SITE_DESCRIPTION with more detailed information

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Artur Komaristych'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_DESCRIPTION =
+  "Passionate software developer pursuing a master's in computer science at the University of Cologne. " +
+  'I enjoy solving complex problems and staying active through badminton, bouldering, and climbing. Discover more about my skills and experiences'
