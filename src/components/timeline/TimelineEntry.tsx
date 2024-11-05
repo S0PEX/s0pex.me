2a67711004ae8a90f7efd6a232900da9ad8aeac4
Artur Komaristych <artur.komaristych@gmail.com>
2024-11-05 13:56:35 +0100

    refactor(components): add margin-bottom to subTitle in TimelineEntry component

--- src/components/timeline/TimelineEntry.tsx
+++ src/components/timeline/TimelineEntry.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@/utils'
+
 type Props = {
   title: string
   subTitle?: string
@@ -10,6 +12,10 @@ function getDate(dateFrom: string, dateTo?: string) {
   return dateTo ? `${dateFrom} - ${dateTo}` : dateFrom
 }
 
+const TimelineDot = () => (
+  <div className="absolute w-3 h-3 bg-gray-400 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+)
+
 const TimelineEntry = ({
   title,
   subTitle,
@@ -18,15 +24,20 @@ const TimelineEntry = ({
   children,
 }: Props) => (
   <li className="mb-10 ms-4">
-    <div className="absolute w-3 h-3 bg-gray-400 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+    <TimelineDot />
     <time className="mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">
       {getDate(dateFrom, dateTo)}
     </time>
-    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+    <h3
+      className={cn(
+        'text-lg mb-1 font-semibold text-gray-900 dark:text-white',
+        subTitle ? 'mb-1' : '',
+      )}
+    >
       {title}
     </h3>
     {subTitle && (
-      <p className="block mb-2 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">
+      <p className="block text-sm font-normal leading-none text-gray-500 dark:text-gray-500">
         {subTitle}
       </p>
     )}
